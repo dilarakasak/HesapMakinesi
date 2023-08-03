d59ab3375d98845bc73d0b92e3face0122cd0cb8
dilarakasak <dilarakasak7@gmail.com>
2023-08-03 17:44:47 +0300

    Finally

--- src/Hesap.js
+++ src/Hesap.js
@@ -2,39 +2,53 @@ import React, { useState } from 'react'
 import './Hesap.css';
 
 function Hesap() {
+    const [deger, setDeger] = useState('');
+
+    const yaz = (value) => {
+        setDeger((prevDeger) => prevDeger + value);
+    }
+
+    const hesapla = () => {
+        setDeger(eval(deger).toString());
+    }
+
+    const temizle = () => {
+        setDeger('');
+    }
+
   return (
     <div className='arkaplan'>
-        <div className='sonuc'></div>
+        <div className='sonuc'>{deger}</div>
 
         <div className='satir'>
-            <button className='button'>7</button> 
-            <button className='button'>8</button> 
-            <button className='button'>9</button> 
-            <button className='button isaret'>/</button> 
+            <button className='button' onClick={() => yaz('7')}>7</button> 
+            <button className='button' onClick={() => yaz('8')}>8</button> 
+            <button className='button' onClick={() => yaz('9')}>9</button> 
+            <button className='button isaret' onClick={() => yaz('/')}>/</button> 
         </div>
         
         <div className='satir'>
-            <button className='button'>4</button>
-            <button className='button'>5</button>
-            <button className='button'>6</button>
-            <button className='button isaret'>*</button>
+            <button className='button' onClick={() => yaz('4')}>4</button>
+            <button className='button' onClick={() => yaz('5')}>5</button>
+            <button className='button' onClick={() => yaz('6')}>6</button>
+            <button className='button isaret' onClick={() => yaz('*')}>*</button>
         </div>
         
         <div className='satir'>
-            <button className='button'>1</button>
-            <button className='button'>2</button>
-            <button className='button'>3</button>
-            <button className='button isaret'>+</button>
+            <button className='button' onClick={() => yaz('1')}>1</button>
+            <button className='button' onClick={() => yaz('2')}>2</button>
+            <button className='button' onClick={() => yaz('3')}>3</button>
+            <button className='button isaret' onClick={() => yaz('+')}>+</button>
         </div>
         
         <div className='satir'>
-            <button className='button'>0</button>
-            <button className='button'>,</button>
-            <button className='button isaret'>=</button>
-            <button className='button isaret'>-</button>
+            <button className='button' onClick={() => yaz('0')}>0</button>
+            <button className='button' onClick={() => yaz('.')}>.</button>
+            <button className='button isaret' onClick={hesapla}>=</button>
+            <button className='button isaret' onClick={() => yaz('-')}>-</button>
         </div>
         
-        <button className='temizle'>TEMİZLE</button>
+        <button className='temizle' onClick={temizle}>TEMİZLE</button>
     </div>
   )
 }
